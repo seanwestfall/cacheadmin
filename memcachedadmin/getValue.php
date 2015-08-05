35abc255a0b4d0962931bfaa387e243b06cc3665
Sean Westfall <seanwestfall.github@gmail.com>
2015-08-04 21:24:55 -0700

    removed old files

--- memcachedadmin/getValue.php
+++
@@ -1,5 +0,0 @@
-<?php
-$m = new Memcached();
-$m->addServer('localhost', 11211);
-echo var_dump($m->get($_GET['key']));
-
--- memcachedadmin/getmemcacheddata.php
+++
@@ -1,8 +0,0 @@
-<?php
-$m = new Memcached();
-$m->addServer('localhost', 11211);
-$statsdata = $m->getStats();
-
-echo json_encode($statsdata);
-?>
-
